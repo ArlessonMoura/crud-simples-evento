32a12cebd0ed12711d88966d0fd7a8ca3bf1b349
Marcos <marcoserick.psv@gmail.com>
2026-07-09 19:10:18 -0300

    começando o casting na listagem de todos os convidados

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs').promises;
 const crypto = require('crypto');
 const path = require('path');
+const { error } = require('console');
 
 const app = express();
 const port = 3000;
@@ -13,4 +14,20 @@ app.use(express.json());
 
 app.listen(port, () => {
   console.log(`API de gestão de convidados iniciada na porta ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("texto");
+});
+
+app.get("/casting", async (req, res) => {
+  console.log("Rota /casting foi chamada");
+
+  try {
+    const data = await fs.readFile(filePath, "utf8");
+
+    res.send(data);
+  } catch (err) {
+    res.status(500).send(`Erro ao ler o arquivo: ${err.message}`);
+  }
 });
